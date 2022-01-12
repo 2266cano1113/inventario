4804a0bf594a8b476adabac72b6913d41a75921d
cano <15670064@iguala.tecnm.mx>
2022-01-12 09:43:03 -0600

    economia historial

--- index.php
+++ index.php
@@ -16,25 +16,9 @@
 
 <body>
      <?php
+    include("conexion.php");
         include("header.php");
     ?>
-    
-    <!-- <header>
-        <nav class="navEscritorio">
-            <a href="#"> <img src="/img/26170916_2103950139631555_2989586965680541136_o.jpg" alt="Logo"></a>
-            <h1>Inventario Dental</h1>
-            <a href="#">Contacto</a>
-            <a href="#">Nosotros</a>
-            <a href="#"><i class="bi bi-facebook"></i></a>
-            <a href="#"><i class="bi bi-instagram"></i></a>
-            <a href="#"><i class="bi bi-whatsapp"></i></a>
-        </nav>
-        <nav class="navMovil">
-            <a href="#"> <img src="/img/26170916_2103950139631555_2989586965680541136_o.jpg" alt="Logo"></a>
-            <h1>Inventario Dental</h1>
-            <i class="bi bi-three-dots-vertical"></i>
-        </nav>
-    </header> -->
     <main>
         <div>
             <h2>Laboratorio</h2>
