3a3d06c6e7e261caa789830fdca0e0a84e4202f4
Diana <dianalaugr13@gmail.com>
2022-01-20 15:39:17 -0600

    provedor terminado

--- editarProveedor.php
+++ editarProveedor.php
@@ -3,113 +3,96 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/variables.css">
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/proveedor.css">
-
-
-    <title>Implants Bionic</title>
-
-
+    <title>Document</title>
 </head>
 
 <body>
-<?php
-    include("header.php");
+    <?php
+    include "header.php";
     ?>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col text-center fondo">
-                <h1>Actualizar Datos</h1>
-            </div>
+    <?php
+    include("conexion.php");
+    $id = $_GET['id'];
+    $idVendedor = $_GET['idVendedor'];
+    $query = "SELECT * FROM proveedor WHERE id = $id";
+    $result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_array($result);
+
+    if (isset($_POST['actualizar'])) {
+        $id = $_POST['id'];
+        $nombre = ucwords(strtolower($_POST['nombre']));
+        $calle = ucwords(strtolower($_POST['calle']));
+        $colonia = ucwords(strtolower($_POST['colonia']));
+        $num = ucwords(strtolower($_POST['num']));
+        $ciudad = ucwords(strtolower($_POST['ciudad']));
+        $telefono = ucwords(strtolower($_POST['telefono']));
+        $email = ucwords(strtolower($_POST['email']));
+
+
+        $insert = "UPDATE proveedor set nombre ='$nombre',calle = '$calle', colonia = '$colonia', num = '$num',ciudad = '$ciudad',
+         telefono = '$telefono', email = '$email' WHERE id= $id";
+                if (mysqli_query($conn, $insert)) {
+            $_SESSION['message'] = 'Registro Actualizado';
+            $_SESSION['message_type'] = 'success';
+            header('Location:proveedor.php?id');
+        } else {
+            echo "El registro no se pudo guardar" . mysqli_error($conn);
+        }
+    }
+    ?>
+    <div class="contenedor">
+
+        <div class="actualizar">
+            <form action="editarProveedor.php?id=<?php echo $row['id'] ?><?php echo $id?>"
+                method="POST">
+                <input class="d-none" type="text" name="idV" value="<?php echo $row['id'] ?>">
+                <label class="d-none" for="">id</label>
+                <input class="d-none" type="text" name="id" value="<?php echo $row['id'] ?>">
+                <div>
+                    <label for="">Nombre:</label>
+                    <input type="text" name="nombre" value="<?php echo $row['nombre'] ?>" require>
+                </div>
+                <div>
+                    <label for="">Calle:</label>
+                    <input type="calle" name="calle" value="<?php echo $row['calle'] ?>" required>
+                </div>
+                <div>
+                    <label for="">Colonia:</label>
+                    <input type="text" name="colonia" value="<?php echo $row['colonia'] ?>" required>
+                </div>
+                <div>
+                    <label for="">Numero:</label>
+                    <input type="text" name="num" value="<?php echo $row['num'] ?>" required>
+                </div>
+                <div>
+                    <label for="">Ciudad:</label>
+                    <input type="text" name="ciudad" value="<?php echo $row['ciudad'] ?>" required>
+                </div>
+                <div>
+                    <label for="">Telefono:</label>
+                    <input type="number" name="telefono" value="<?php echo $row['telefono'] ?>" required>
+                </div>
+                <div>
+                    <label for="">Email:</label>
+                    <input type="email" name="email" value="<?php echo $row['email'] ?>" required>
+                </div>
+              
+                <button class="btn-actualizar btn btn-secondary" type="submit" name="actualizar">Actualizar</button>
+            </form>
+            
         </div>
 
-
-        <?php
-        include("conexion.php");
-        if (isset($_GET['id'])) {
-            $id = $_GET['id'];
-
-            $query = "SELECT * FROM proveedor WHERE id = $id";
-            $result = mysqli_query($conn, $query);
-
-            if (mysqli_num_rows($result) == 1) {
-                $row = mysqli_fetch_array($result);
-
-                $id = $row['id'];
-                $nombre = $row['nombre'];
-                $calle = $row['calle'];
-                $num = $row['num'];
-                $colonia = $row['colonia'];
-                $ciudad = $row['ciudad'];
-                $telefono = $row['telefono'];
-                $email = $row['email'];
-
-            }
-        }
-        if (isset($_POST['update'])) {
-            $id = $_GET['id'];
-            $nombre = $_POST['nombre'];
-            $calle = $_POST['calle'];
-            $num = $_POST['num'];
-            $colonia = $_POST['colonia'];
-            $ciudad = $_POST['ciudad'];
-            $telefono = $_POST['telefono'];
-            $email = $_POST['email'];
-
-            $update = "UPDATE descripcion set nombre = '$nombre', calle ='$calle', num = '$num', colonia = '$colonia',
-             ciudad = '$ciudad', telefono = '$telefono',email ='$email',  WHERE id= $id";
-            mysqli_query($conn, $update);
-            $_SESSION['message'] = 'Registro actualizado exitosamente';
-            $_SESSION['message_type'] = 'info';
-            header('Location:proveedor.php?id='.$id);
-        }
-        ?>
-       
-                        <form name="form" action="updateDescripcion.php?id=<?php echo $_GET['id']; ?>" onsubmit="return validarform()" method="POST">
-                            <label for="" class="d-none">ID: </label>
-                            <div class="form-group">
-                                <input type="number" name="id" value="<?php echo $id; ?>" class="form-control d-none" placeholder="Actualiza ID" autocomplete="off" autofocus>
-                            </div>
-                            <label for="" class="d-none">nombre:</label>
-                            <div class="form-group">
-                                <input type="text" name="nombre" value="<?php echo $nombre; ?>" class="form-control d-none" placeholder="Actualiza ID_Asociado" autocomplete="off" required>
-                            </div><label for=""> calle:</label>
-                            <div class="form-group">
-                                <input type="text" name="calle" value="<?php echo $calle; ?>" class="form-control" placeholder="Actualiza Descripcion" autocomplete="off" required>
-                            </div>
-                            </div><label for=""> num:</label>
-                            <div class="form-group">
-                                <input type="text" name="num" value="<?php echo $num; ?>" class="form-control" placeholder="Actualiza Descripcion" autocomplete="off" required>
-                            </div></div><label for=""> colonia:</label>
-                            <div class="form-group">
-                                <input type="text" name="colonia" value="<?php echo $colonia; ?>" class="form-control" placeholder="Actualiza Descripcion" autocomplete="off" required>
-                            </div></div><label for=""> ciudad:</label>
-                            <div class="form-group">
-                                <input type="text" name="ciudad" value="<?php echo $ciudad; ?>" class="form-control" placeholder="Actualiza Descripcion" autocomplete="off" required>
-                            </div></div><label for=""> telefono:</label>
-                            <div class="form-group">
-                                <input type="text" name="telefono" value="<?php echo $telefono; ?>" class="form-control" placeholder="Actualiza Descripcion" autocomplete="off" required>
-                            </div></div><label for=""> email:</label>
-                            <div class="form-group">
-                                <input type="text" name="email" value="<?php echo $email; ?>" class="form-control" placeholder="Actualiza Descripcion" autocomplete="off" required>
-                            </div>
-
-                            <button class="btn btn-secondary w-100 mt-3" name="update">
-                                Actualizar
-                            </button>
-                            <a class="btn btn-secondary w-100 mt-3" href="proveedor.php?id=<?php echo $id ?>">Cancelar</a>
-                        </form>
-                  
-
+    </div>
     <?php
     include("footer.php");
     ?>
-    </div>
     <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 
